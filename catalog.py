c45a5d57515e5f0c8cd51e3928d04ed76644c48f
Von <vonzedric012@gmail.com>
2025-06-21 14:21:43 +0800

    added update, parse, and print methods

--- catalog.py
+++ catalog.py
@@ -1,14 +1,30 @@
-from json import loads
+import json
 
 class Catalog:
     def __init__(self, file_path):
         file = open(file_path, 'r')
-        data = loads(file.read())
+        data = json.loads(file.read())
 
         self.title = data["title"]
         self.author = data["author"]
         self.genre = data["genre"]
         self.ref = data["reference"]
-        self.path = file_path
+        self.__path = file_path
 
+    def update(self):
+        file = open(self.__path, 'w')
+        data = json.dumps(self.__parse())
+        file.write(data)
+        
+    def __parse(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "genre": self.genre,
+            "reference": self.ref
+        }
     
+    def print(self):
+        data = self.__parse()
+        data["path"] = self.__path
+        print(data)
