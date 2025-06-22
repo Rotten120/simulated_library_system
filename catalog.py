9ab510f09cdd8e61074147b846c874ab3f963ed0
Von <vonzedric012@gmail.com>
2025-06-22 18:49:31 +0800

    added methods for adding, editing, and removing catalogs

--- catalog.py
+++ catalog.py
@@ -1,23 +1,52 @@
 import json
+import os
 
 class Catalog:
-    def __init__(self, file_path):
+    # CLASS INITIALIZE
+    
+    def __init__(self, data, directory):
+        self.id = data["id"]
+        self.__path = directory + "\\" + str(self.id) + ".txt"
+        self.edit(data)
+
+    def imp(file_path):
         file = open(file_path, 'r')
         data = json.loads(file.read())
 
-        self.title = data["title"]
-        self.author = data["author"]
-        self.genre = data["genre"]
-        self.ref = data["reference"]
-        self.__path = file_path
+        directory = file_path[:file_path.rfind('\\')]
+        catalog = Catalog(data, directory)
+        
+        file.close()
+        return catalog
 
-    def update(self):
+    # OBJECTS MODIFY
+
+    def edit(self, data):
+        if self.id == data["id"]:
+            self.title = data["title"]
+            self.author = data["author"]
+            self.genre = data["genre"]
+            self.ref = data["reference"]
+
+    # FILE MANAGEMENT
+
+    def write(self):
         file = open(self.__path, 'w')
-        data = json.dumps(self.__parse())
-        file.write(data)
+        file.write(json.dumps(self.__parse()))
+        file.close()
+
+    def rmv(self):
+        if os.path.isfile(self.__path):
+            os.remove(self.__path)
+
+    # GET METHODS
+
+    def get_path(self):
+        return self.__path
         
     def __parse(self):
         return {
+            "id": self.id,
             "title": self.title,
             "author": self.author,
             "genre": self.genre,
--- catalog_management.py
+++ catalog_management.py
@@ -1,12 +1,40 @@
 import os
 from catalog import *
 
-class CatalogManagement:
-    def __init__(self):
-        catalogs = []
-
-    def get_catalogs(self, directory):
-        for entry in os.scandir(directory):
+class CatMgr:    
+    def __init__(self, directory):
+        self.dir = directory
+        self.catalogs = self.__get_catalogs()
+        
+    def __get_catalogs(self):
+        cats = {}
+        for entry in os.scandir(self.dir):
             if entry.is_file():
-                cat = Catalog(entry.path)
-                catalogs.append(cat)
+                cat = Catalog.imp(entry.path)
+                cats[cat.id] = cat
+        return cats
+
+    def add(self, data):
+        data_id = data["id"]
+        catalog = Catalog(data)
+
+        if not(data_id in self.catalogs):
+            catalog.write()
+            self.catalogs[data_id] = catalog
+        else:
+            print("Catalog ID already in directory")
+        
+    def edit(self, data):
+        data_id = data["id"]
+        if data_id in self.catalogs:
+            self.catalogs[data_id].edit(data)
+        else:
+            print("Catalog ID does not exist")
+
+    def remove(self, id_num):
+        if id_num in self.catalogs:
+            self.catalogs[id_num].rmv()
+            del self.catalogs[id_num]
+        else:
+            print("Catalog ID does not exist")
+
