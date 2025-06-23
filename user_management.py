5c10b4a6414716c08d07376f91b68b44365955c7
Von <vonzedric012@gmail.com>
2025-06-23 14:48:37 +0800

    added password verification for edit and rmv methods. add username as unique value aside from id in add method

--- user_management.py
+++ user_management.py
@@ -2,18 +2,41 @@ from user import User
 from file_manager import FileManager
 
 class UserMgr(FileManager):
+    #username and id has to be unique
+    def add(self, data):
+        results = self.search("username", data["username"])
+        if len(results) == 0:
+            super().add(data)
+        else:
+            print("Username already exists")
+
+    #modified version of edit that also validates password
+    def edit(self, item_id, data, password):
+        if not(item_id in self.items):
+            print("Item ID does not exist in directory")
+            return
+
+        item = self.items[item_id]
+        if item.password_check(password):
+            item.edit(data)
+        else:
+            print("Wrong password")
+
+    #modified version of edit that also validates password
+    def remove(self, item_id, password):
+        if not(item_id in self.items):
+            print("Item ID does not exist in directory")
+            return
+        
+        item = self.items[item_id]
+        if item.password_check(password):
+            item.rmv(data)
+            del item
+        else:
+            print("Wrong password")
+    
     def import_item(self, file_path):
         return User.imp(file_path)
 
     def create_item(self, data):
         return User(data, self.dir)
-
-data = {
-    "id": 6410011,
-    "username": "VONVON",
-    "password": "asdjalskd",
-    "privilege": "STUDENT"
-}
-
-a = UserMgr("user_lists")
-a.add(data)
