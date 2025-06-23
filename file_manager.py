f729b2e73f985cfa43a445d25420e1ea074ad491
Von <vonzedric012@gmail.com>
2025-06-24 07:37:30 +0800

    added generation of valid id method

--- file_manager.py
+++ file_manager.py
@@ -16,17 +16,11 @@ class FileManager(ABC):
         return items
 
     def add(self, data):
-        item_id = data["id"]
-
-        while item_id in self.items:
-            item_id = random.randrange(0, 999999)
-
-        data["id"] = item_id
-        
+        item_id = self.generate_id(data["id"])
         item = self.create_item(data)
-        item.write()
         
         self.items[item_id] = item
+        self.items[item_id].write()
         return item_id
 
     def edit(self, item_id, data):
@@ -35,8 +29,6 @@ class FileManager(ABC):
             item.edit(data)
             item.write()
             return True
-
-        print("Item ID does not exists in directory")
         return False
 
     def remove(self, item_id):
@@ -44,21 +36,24 @@ class FileManager(ABC):
             self.items[item_id].rmv()
             del self.items[item_id]
             return True
-
-        print("Item ID does not exists in directory")
         return False
 
     def search(self, key, prompt):
-        results = {}
+        results = []
         for item_id in self.items:
             if self.items[item_id].parse()[key] == prompt:
-                results[item_id] = self.items[item_id]
+                results.append(self.items[item_id])
         return results
 
     def print(self):
         for item_id in self.items:
             print(end = "\n\n")
             self.items[item_id].print()
+
+    def generate_id(self, item_id):
+        while item_id in self.items:
+            item_id = random.randrange(0, 999999)
+        return item_id
 
     @abstractmethod
     def import_item(self, file_path):
