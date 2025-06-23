661cf3b68a1c7cf913b49e93738f58fe665cfa37
Von <vonzedric012@gmail.com>
2025-06-24 07:36:55 +0800

    simplify code

--- account_management.py
+++ account_management.py
@@ -2,43 +2,30 @@ from account import Acc
 from file_manager import FileManager
 
 class AccMgr(FileManager):
-    #username and id has to be unique
     def add(self, data):
         results = self.search("username", data["username"])
         if len(results) == 0:
             return super().add(data)
-
-        print("Username already exists")
         return False
 
-    #modified version of edit that also validates password
     def edit(self, item_id, data, password):
-        if not(item_id in self.items):
-            print("Item ID does not exist in directory")
+        try:
+            item = self.items[item_id]
+        except KeyError:
             return False
 
-        item = self.items[item_id]
         if item.password_check(password):
-            val = item.edit(data)
-            item.write()
-            return val
-        
-        print("Wrong password")
+            return super().edit(item_id, data)
         return False
     
-    #modified version of edit that also validates password
     def remove(self, item_id, password):
-        if not(item_id in self.items):
-            print("Item ID does not exist in directory")
+        try:
+            item = self.items[item_id]
+        except KeyError:
             return False
-        
-        item = self.items[item_id]
+
         if item.password_check(password):
-            item.rmv(data)
-            del item
-            return True
-        
-        print("Wrong password")
+            return super().remove(item_id)
         return False
     
     def import_item(self, file_path):
