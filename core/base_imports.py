08d3e4d552047cf9a9a74ad94941076df58df55a
Von <vonzedric012@gmail.com>
2025-07-20 11:14:50 +0800

    removed files from working directory, not deleted in the local files

--- core/base_imports.py
+++
@@ -1,6 +0,0 @@
-from table.account import Account
-from table.catalog import Catalog
-from table.transact import Transaction
-from table_manager.account_manager import AccountManager
-from table_manager.catalog_manager import CatalogManager
-from table_manager.transact_manager import TransactManager
--- table_manager/catalog_manager.py
+++
@@ -1,6 +0,0 @@
-from table.catalog import Catalog
-from table_manager.table_manager import TableManager
-
-class CatalogManager(TableManager):
-    pass
-
