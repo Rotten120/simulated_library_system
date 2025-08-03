3700f115bba1495c9f918a0c1c02e6ae7c93df50
Von <vonzedric012@gmail.com>
2025-08-03 21:24:16 +0800

    added env variable for local database user

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from core.libsys import LibSys
 from pages import *
 
+# remove this <ROOT> and replace all of its instances
+# with the informtion necessary before
+# executing in your own device
+from ROOT import Root
+
 if __name__ == "__main__":
     pages = {
         "main_menu": MainMenu,
@@ -11,11 +16,11 @@ if __name__ == "__main__":
         "return_cat": ReturnCatalog,
         "acc_setting": AccSetting
     }
-    
+
     LibSys.init(
         "localhost",
-        "root",
-        "RottenFlesh@123",
+        Root.user,      # replace with your superuser
+        Root.password,  # replace with your superuser's password
         "sim_lib_sys",
         pages
     )
