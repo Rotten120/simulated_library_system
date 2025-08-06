3d10b4a35f5ff501a0ce0f2c6a79700c2261c875
Von <vonzedric012@gmail.com>
2025-08-06 14:39:42 +0800

    adjusted the expected outcome based on recent changes in login

--- test/login_test.py
+++ test/login_test.py
@@ -5,17 +5,17 @@ class LoginTest(unittest.TestCase):
     def test_valid_login(self):
         inp = ("Zedric", "secret")
         output = "Successfully logged as " + inp[0]
-        self.assertEqual(Login.logic(inp), output)
+        self.assertEqual(Login.logic(inp), (output, 1))
 
     def test_invalid_username(self):
         inp = ("Zeds", "secret")
         output = "Username does not exist"
-        self.assertEqual(Login.logic(inp), output)
+        self.assertEqual(Login.logic(inp), (output, 0))
         
     def test_invalid_password(self):
         inp = ("Zedric", "hehe")
         output = "Password is incorrect"
-        self.assertEqual(Login.logic(inp), output)
+        self.assertEqual(Login.logic(inp), (output, 0))
 
 if __name__ == "__main__":
     main.libsys_init()
