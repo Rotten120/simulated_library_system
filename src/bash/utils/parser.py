8a124009d95ace680d5087ee2044cc607360358f
Rotten120 <thenanokid@gmail.com>
2025-08-11 15:52:22 +0800

    fixed code format in parser

--- src/bash/utils/parser.py
+++ src/bash/utils/parser.py
@@ -10,14 +10,19 @@ class ConfigParser:
         for line in lines:
             line = line[:-1] if line[-1:] == "\n" else line
             equal_pos = line.find('=')
-            
-            if line == "" or line[0] == "#" or not(0 < equal_pos < len(line)):
+
+            if line == "" or line[0] == "#":
                 continue
-            
+
             if line[0] == '[' and line[-1:] == ']':
                 namespace = line[1:-1]
                 config[namespace] = {}
-            else:
-                config[namespace][line[:pos]] = line[pos + 1:]
+
+            if (0 < equal_pos < len(line)):
+                key = line[:equal_pos]
+                val = line[equal_pos + 1:]
+                if line[equal_pos + 1] == '"' and line[-1] == '"':
+                    val = val[1:-1]
+                config[namespace][key] = val
         return config
 
